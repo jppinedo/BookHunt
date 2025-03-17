83e1e2550c389e84d1c73d542a87be582c83f5e6
juanpablo.pinedo <juanpablo.pinedo@providaexternos.cl>
2025-03-17 19:14:44 -0300

    Fixed loading spinner for infinite scrolling

--- src/components/custom/Search/LoadingResults.jsx
+++ src/components/custom/Search/LoadingResults.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { CircularProgress, Paper } from "@mui/material";
 
 const LoadingResults = ({ sx }) => (
-<Paper sx={{ 
-    p: 1.5, 
-    borderRadius: 2, 
-    marginLeft: 'auto', 
-    marginRight: 'auto', 
-    maxWidth: 'fit-content', 
-    backgroundColor: 'rgba(255, 255, 255, 0.8)', 
-    flex: '0 0 100%',
-    ...sx }}
-  >
-    <CircularProgress color="primary" size={30} />
-  </Paper>
+  <div style={{width:'100%', display: 'flex', justifyContent: 'center'}} >
+    <Paper sx={{ 
+        p: 1.5, 
+        borderRadius: 2, 
+        maxWidth: 'fit-content', 
+        backgroundColor: 'rgba(255, 255, 255, 0.8)', 
+        ...sx }}
+      >
+        <CircularProgress color="primary" size={30} />
+    </Paper>
+  </div>
 );
 
 export default LoadingResults;
